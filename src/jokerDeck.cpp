768defd33919330a191ea6c4b44faf6a56461ddc
johnnnathan <johnnnathan@proton.me>
2024-07-16 17:53:29 +0300

    'Fished' the joker deck and fixed some minor bugs in the same file

--- src/jokerDeck.cpp
+++ src/jokerDeck.cpp
@@ -1,18 +1,27 @@
 #include "jokerDeck.h"
 #include "joker.h" 
 #include "tools.h"
+#include <cstddef>
 
 const std::string FULL_JOKER_DECK = "You can not add any more jokers, at capacity";
 const std::string EMPTY_JOKER_DECK = "No Jokers, cannot remove";
 const std::string JOKER_NOT_FOUND = "Joker not found, cannot remove";
+const std::string ERROR_ADDING_JOKER = "Could not add joker, try again";
+
+
 void JokerDeck::addJoker(Joker* joker){
   if (!(getJokerCount() < getDeckSize())){
     printError(FULL_JOKER_DECK);
     return;
   }
-  jokers[getJokerCount()] = joker;
-  incrementCurrent();
-
+  for (int i = 0; i <= getDeckSize(); i++){
+    if(jokers[i] == NULL){
+      jokers[getJokerCount()] = joker;
+      incrementCurrent();
+      return;
+    }
+  }
+  printError(ERROR_ADDING_JOKER);
 }
 
 void JokerDeck::removeJoker(std::string jokerName){
@@ -20,15 +29,14 @@ void JokerDeck::removeJoker(std::string jokerName){
     printError(EMPTY_JOKER_DECK);
     return;
   }
-  for (int i = 0; i < getJokerCount(); i++){
+  for (int i = 0; i <= getDeckSize(); i++){
     if (jokers[i]->getName() == jokerName){
       delete jokers[i];
+      decrementCurrent();
       return;
     }
-
   }
   printError(JOKER_NOT_FOUND);
-  decrementCurrent();
 }
 
 void JokerDeck::decrementCurrent(){
