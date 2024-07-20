dc770e8f460e0bbbeb71c5fb6b3477bd23bd8b38
johnnnathan <johnnnathan@proton.me>
2024-07-20 16:14:57 +0300

    Added pair logic, need to implement the specific hands but should not be difficult

--- src/hand.cpp
+++ src/hand.cpp
@@ -1,3 +1,81 @@
 #include "hand.h"
+#include "points.h"
+#include <array>
+#include <utility>
 
 
+Hand::Hand(const std::array<PlayingCard, 5>& cards, int multiplier, int chips)
+    : cards(cards), points{multiplier, chips} {
+}
+
+bool Hand::isHighCard() const{
+
+}
+bool Hand::isPair() const{
+
+}
+bool Hand::isTwoPair() const{
+
+}
+bool Hand::isThreeOfAKind() const{
+
+}
+bool Hand::isFourOfAKind() const{
+
+}
+bool Hand::isFiveOfAKind() const{
+
+}
+bool Hand::isFullHouse() const{
+
+}
+bool Hand::isFlush() const{
+
+}
+bool Hand::isStraight() const{
+
+}
+bool Hand::isStraightFlush() const{
+
+}
+bool Hand::isRoyalFlush() const{
+
+}
+bool Hand::isFlushHouse() const{
+
+}
+bool Hand::isFlushFive() const{
+
+}
+    
+
+
+std::pair<std::pair<int,int>,std::vector<bool>> Hand::findPairs(){
+  std::pair<int,int> pairCount = {0,0};
+  int cardCount = cards.size(); //might be able to use sizeof(cards)/sizeof(PlayingCard)
+  std::vector<bool> positions(cardCount, false);
+  int ranks[13] = {0};
+
+  for (int i = 0; i < cardCount; i++){
+    ranks[cards[i].getRank()] += 1;
+  }
+
+  for (int i = 0; i < 13; i++){
+    if (ranks[i] > pairCount.first){
+      pairCount.second = pairCount.first;
+      pairCount.first = ranks[i];
+    }
+    else if (ranks[i] > pairCount.second) {
+      pairCount.second = ranks[i];
+    }
+  }
+
+  for (int i = 0; i < cardCount; i++){
+    if(ranks[cards[i].getRank()] == pairCount.first || ranks[cards[i].getRank()] == pairCount.second){
+      positions[i] = true;
+    }
+  }
+
+  std::pair<std::pair<int, int>, std::vector<bool>> total = {pairCount,positions};
+  return total;
+}
--- src/hand.h
+++ src/hand.h
@@ -2,14 +2,18 @@
 #define HAND_H
 
 #include "handTypes.h"
+#include <array>
 #include <utility>
+#include <vector>
 #include "points.h"
 #include "playingCard.h"
 
 class Hand {
   private:
+    Points points;
+    std::array<PlayingCard, 5> cards;
     bool isHighCard() const;
-    bool isPair();  const 
+    bool isPair() const;
     bool isTwoPair() const;
     bool isThreeOfAKind() const;
     bool isFourOfAKind() const;
@@ -21,12 +25,11 @@ class Hand {
     bool isRoyalFlush() const;
     bool isFlushHouse() const;
     bool isFlushFive() const;
+    std::pair<std::pair<int,int>, std::vector<bool>> findPairs();
   public:
     std::pair<HandType, Points> evaluate();
-    Hand(const std::array<PlayingCard, 5>& cards, int multiplier = 1, int chips = 0);
+    Hand(const std::array<PlayingCard, 5>& cards, int multiplier, int chips);
     void print() const;
 };
-
-
 
 #endif // !HAND_H
