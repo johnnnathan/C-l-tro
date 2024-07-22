536713d3f2eebfcb12fe8706963b7b39d6c2ddf6
johnnnathan <johnnnathan@proton.me>
2024-07-22 17:36:46 +0300

    changed some settings in my code editor configurations, making sure that nothing is lost

---
+++ build/CMakeFiles/MyExecutable.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/dimi/Desktop/Projects/C-l-tro/src/deck.cpp" "CMakeFiles/MyExecutable.dir/src/deck.cpp.o" "gcc" "CMakeFiles/MyExecutable.dir/src/deck.cpp.o.d"
+  "/Users/dimi/Desktop/Projects/C-l-tro/src/hand.cpp" "CMakeFiles/MyExecutable.dir/src/hand.cpp.o" "gcc" "CMakeFiles/MyExecutable.dir/src/hand.cpp.o.d"
+  "/Users/dimi/Desktop/Projects/C-l-tro/src/joker.cpp" "CMakeFiles/MyExecutable.dir/src/joker.cpp.o" "gcc" "CMakeFiles/MyExecutable.dir/src/joker.cpp.o.d"
+  "/Users/dimi/Desktop/Projects/C-l-tro/src/jokerDeck.cpp" "CMakeFiles/MyExecutable.dir/src/jokerDeck.cpp.o" "gcc" "CMakeFiles/MyExecutable.dir/src/jokerDeck.cpp.o.d"
+  "/Users/dimi/Desktop/Projects/C-l-tro/src/main.cpp" "CMakeFiles/MyExecutable.dir/src/main.cpp.o" "gcc" "CMakeFiles/MyExecutable.dir/src/main.cpp.o.d"
+  "/Users/dimi/Desktop/Projects/C-l-tro/src/playingCard.cpp" "CMakeFiles/MyExecutable.dir/src/playingCard.cpp.o" "gcc" "CMakeFiles/MyExecutable.dir/src/playingCard.cpp.o.d"
+  "/Users/dimi/Desktop/Projects/C-l-tro/src/tools.cpp" "CMakeFiles/MyExecutable.dir/src/tools.cpp.o" "gcc" "CMakeFiles/MyExecutable.dir/src/tools.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/MyExecutable.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyExecutable.dir/src/deck.cpp.o"
+  "CMakeFiles/MyExecutable.dir/src/deck.cpp.o.d"
+  "CMakeFiles/MyExecutable.dir/src/hand.cpp.o"
+  "CMakeFiles/MyExecutable.dir/src/hand.cpp.o.d"
+  "CMakeFiles/MyExecutable.dir/src/joker.cpp.o"
+  "CMakeFiles/MyExecutable.dir/src/joker.cpp.o.d"
+  "CMakeFiles/MyExecutable.dir/src/jokerDeck.cpp.o"
+  "CMakeFiles/MyExecutable.dir/src/jokerDeck.cpp.o.d"
+  "CMakeFiles/MyExecutable.dir/src/main.cpp.o"
+  "CMakeFiles/MyExecutable.dir/src/main.cpp.o.d"
+  "CMakeFiles/MyExecutable.dir/src/playingCard.cpp.o"
+  "CMakeFiles/MyExecutable.dir/src/playingCard.cpp.o.d"
+  "CMakeFiles/MyExecutable.dir/src/tools.cpp.o"
+  "CMakeFiles/MyExecutable.dir/src/tools.cpp.o.d"
+  "MyExecutable"
+  "MyExecutable.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MyExecutable.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
